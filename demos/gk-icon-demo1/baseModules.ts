084117614e0e1b1b173ba2f23db5c456fb1d77c9
hanchaowei <hanchaowei@uniontech.com>
2021-08-16 18:11:33 +0800

    update

---
+++ demos/gk-icon-demo1/baseModules.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+const baseModules = [
+    BrowserModule,
+    BrowserAnimationsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+]
+
+export {
+    baseModules
+}
