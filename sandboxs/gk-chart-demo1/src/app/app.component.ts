5aa110c71e1e0164f3d381e5ad787c196067984e
hanchaowei <hanchaowei@uniontech.com>
2021-08-17 15:04:01 +0800

    update

--- sandboxs/gk-chart-demo1/src/app/app.component.ts
+++ sandboxs/gk-chart-demo1/src/app/app.component.ts
@@ -1,9 +1,60 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from '@angular/core'
+import { GKChartPieConfig, GKChartLineConfig } from 'ng-goku'
 
 @Component({
-    selector: "app-root",
-    template: "",
+    selector: 'app-root',
+    template: `
+    <span>默认样式：</span>
+    <div style="width: 100%; height: 400px">
+        <gk-chart-line #chart1 [data]="lineData1"></gk-chart-line>
+    </div>
+    <br/>
+    <span>支持配置颜色，折线样式，及y轴起点</span>
+    <div style="width: 100%; height: 400px">
+        <gk-chart-line #chart2 [data]="lineData2" [config]="lineConfig2"></gk-chart-line>
+    </div>
+    `,
 })
 export class AppComponent {
+    @ViewChild('chart1') chart1: any
+    @ViewChild('chart2') chart2: any
 
+    lineData1
+    lineData2
+
+    lineConfig2: GKChartLineConfig = {
+        color: ['#3ba272', '#ea7ccc'],
+        type: 'line',
+        yAxisScale: true,
+    }
+
+    constructor() {
+        this.lineData1 = {
+            xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+            series: [
+                {
+                    name: '邮件营销',
+                    value: [120, 132, 101, 134, 90, 230, 210],
+                },
+            ],
+        }
+        this.lineData2 = {
+            xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+            series: [
+                {
+                    name: '邮件营销',
+                    value: [120, 132, 101, 134, 90, 230, 210],
+                },
+                {
+                    name: '联盟广告',
+                    value: [220, 182, 191, 234, 290, 330, 310],
+                },
+            ],
+        }
+        // 如果图表大小，样式不正确，可以执行chart实例的resize方法
+        setTimeout(() => {
+            this.chart1.resize()
+            this.chart2.resize()
+        }, 0)
+    }
 }
--- sandboxs/gk-chart-demo2/src/app/app.component.ts
+++ sandboxs/gk-chart-demo2/src/app/app.component.ts
@@ -1,9 +1,99 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from '@angular/core'
+import { GKChartPieConfig, GKChartLineConfig } from 'ng-goku'
 
 @Component({
-    selector: "app-root",
-    template: "",
+    selector: 'app-root',
+    template: `
+    <span>单个饼图：</span>
+    <div style="width: 400px; height: 147px">
+        <gk-chart-pie #chart1 [data]="pieData1" ></gk-chart-pie>
+    </div>
+    <br/>
+    <span>嵌套饼图</span>
+    <div style="width: 400px; height: 147px">
+        <gk-chart-pie #chart2 [data]="pieData2" [config]="pieConfig2"></gk-chart-pie>
+    </div>
+
+    `,
 })
 export class AppComponent {
+    @ViewChild('chart1') chart1: any
+    @ViewChild('chart2') chart2: any
 
+    pieData1
+    pieData2
+
+
+    pieConfig2: GKChartPieConfig = {
+        width: 400,
+        height: 147,
+        center: [100, '50%'],
+        orient: 'vertical',
+        colorGroup: ['#4F9AFF', '#FFB257', '#ee6666', '#73c0de', '#3ba272', '#fc8452', '#9a60b4', '#ea7ccc'],
+        legendFormatter: (name, value, percent, sum) => {
+            return `${name}：${percent}；总和：${sum}`
+        },
+    }
+
+    constructor() {
+        this.pieData1 = {
+            series: [
+                {
+                    name: '男',
+                    value: [
+                        57,
+                    ],
+                },
+                {
+                    name: '女',
+                    value: [
+                        47,
+                    ],
+                },
+                {
+                    name: '未知',
+                    value: [
+                        123,
+                    ],
+                },
+            ],
+        }
+        this.pieData2 = {
+            series: [
+                {
+                    name: 'QQ',
+                    group: '腾讯',
+                    value: [
+                        57,
+                    ],
+                },
+                {
+                    name: '微信',
+                    group: '腾讯',
+                    value: [
+                        87,
+                    ],
+                },
+                {
+                    name: '淘宝',
+                    group: '阿里',
+                    value: [
+                        21,
+                    ],
+                },
+                {
+                    name: '支付宝',
+                    group: '阿里',
+                    value: [
+                        119,
+                    ],
+                },
+            ],
+        }
+        // 如图表大小，样式不正确，可以执行chart实例的resize方法
+        setTimeout(() => {
+            this.chart1.resize()
+            this.chart2.resize()
+        }, 0)
+    }
 }
