fbdbcb870924afcdc882849a1fc923542c7276e6
hanchaowei <hanchaowei@uniontech.com>
2021-08-17 15:12:16 +0800

    update

--- create.js
+++ create.js
@@ -1,10 +1,11 @@
 const fs = require('fs-extra')
 
+fs.removeSync('./sandboxs/')
+
 const demoPaths = fs.readdirSync('./demos/')
 for (const demoPath of demoPaths) {
     const demo = fs.statSync('./demos/' + demoPath)
     if (demo.isDirectory()) {
-        fs.removeSync('./sandboxs/' + demoPath)
         fs.ensureDirSync('./sandboxs/' + demoPath)
         fs.copySync('./template/', './sandboxs/' + demoPath)
         fs.copySync('./demos/' + demoPath + '/', './sandboxs/' + demoPath + '/src/app')
